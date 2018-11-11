a32869f385e95777603ef59f5ffe6411c0ca5406
ohoseki <ohoseki@hotmail.com>
2018-11-10 22:40:16 -0200

    teste

--- MICHEL_FIAP_SCJ/SCJ Fundamentos Aula 2/src/ex4/ClassificacaoClientes.java
+++ MICHEL_FIAP_SCJ/SCJ Fundamentos Aula 2/src/ex4/ClassificacaoClientes.java
@@ -3,7 +3,7 @@ package ex4;
 public enum ClassificacaoClientes {
 	
 	POTENCIAL(400000,500000), MEDIO(200000,400000), BAIXO(0,200000);
-	
+	//testes
 	private double faixaInicial;
 	private double faixaFinal;
 	
